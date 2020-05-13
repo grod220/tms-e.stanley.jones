dd4caf40e86d27a854a5e8c115c89503d141167b
Gabe Rodriguez <gabe@spotify.com>
2020-05-13 17:10:33 +0200

    Menu route

--- app/pages/menu.jsx
+++ app/pages/menu.jsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import TopText from '../components/menu/top-text';
+import ShortHero from '../components/shared/short-hero';
+import MenuHero from '../components/menu/food-image-tiles.jpg';
+import MenuGrid from '../components/menu/menu-grid';
 
 export default function Menu() {
   return (
@@ -8,7 +11,9 @@ export default function Menu() {
         <title>Menu :: The Meatball Stoppe</title>
         <meta name="description" content="We have the best Italian food you'll find in Orlando" />
       </Head>
+      <ShortHero image={MenuHero} headline="Our delicious menu" />
       <TopText />
+      <MenuGrid />
     </div>
   );
 }
