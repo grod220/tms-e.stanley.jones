7c604b69ddc2c492ee18f5959da906b15b1dad05
Gabe Rodriguez <gabe@spotify.com>
2020-05-13 15:29:57 +0200

    Changing up file names and use of index.jsx

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,10 +1,10 @@
 import Head from 'next/head';
-import Overlay from '../components/homepage/modal-overlay/modal-overlay';
-import BigHero from '../components/homepage/hero/big-hero';
+import Overlay from '../components/homepage/modal-overlay';
+import BigHero from '../components/homepage/big-hero';
 import OrderBar from '../components/homepage/order-bar';
-import SocialBar from '../components/homepage/social-bar/social-bar';
-import MenuPreview from '../components/homepage/menu-preview/menu-preview';
-import TestimonialTaster from '../components/homepage/testimonial-taster/testimonial-taster';
+import SocialBar from '../components/homepage/social-bar';
+import MenuPreview from '../components/homepage/menu-preview';
+import TestimonialTaster from '../components/homepage/testimonial-taster';
 
 export default function Index() {
   return (
