456fe4076de59ec21afad7ba9064bd4bf91281b1
Gabe Rodriguez <gabe@spotify.com>
2020-05-13 20:16:54 +0200

    Integrate Google Analytics

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../public/global-styles.css';
 import Navigation from '../components/navigation';
+import { initializeGoogleAnalytics } from '../utilities/google-analytics';
+
+initializeGoogleAnalytics();
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
