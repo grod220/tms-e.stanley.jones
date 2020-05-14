2fcd50e2e0ff16cfcdecb6e63b31c4ec794d52a9
Gabe Rodriguez <gabe@spotify.com>
2020-05-14 18:24:54 +0200

    Able to build functions

--- app/next-env.d.ts
+++ app/next-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module '*.jpg' {
-  const value: any;
-  export = value;
-}
-
-declare module '*.png' {
-  const value: any;
-  export = value;
-}
